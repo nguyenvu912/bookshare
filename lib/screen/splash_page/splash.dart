f85592716e373a11e49f156c203fa70301467261
Vũ Nguyễn <vunguyen@Vus-MacBook-Pro.local>
2022-10-30 17:46:16 +0700

    push splash_page

--- lib/screen/splash_page/splash.dart
+++ lib/screen/splash_page/splash.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
 
+import 'package:bookshare/app/constant/app_images.dart';
+import 'package:bookshare/app/constant/constant_barrel.dart';
 import 'package:bookshare/screen/auth/login_page/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:splash_screen_view/SplashScreenView.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -14,8 +17,6 @@ class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
     super.initState();
-
-    Timer(const Duration(seconds: 5), () => const LoginPage());
   }
 
   @override
@@ -25,7 +26,13 @@ class _SplashPageState extends State<SplashPage> {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LoginPage(),
+      home: SplashScreenView(
+        navigateRoute: const LoginPage(),
+        duration: 3000,
+        imageSize: 130,
+        imageSrc: AppImages.icLogoApp,
+        backgroundColor: AppColors.white,
+      ),
     );
   }
 }
